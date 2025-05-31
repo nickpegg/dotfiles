1f8952362ca6191b93aadd04dd899c30dcd6aea0
Nick Pegg <nick@nickpegg.com>
2025-05-31 13:27:24 -0700

    vim tweaks

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,6 @@
 -- vim compat
 vim.cmd("set runtimepath^=~/.vim runtimepath+=~/.vim/after")
 vim.cmd("let &packpath = &runtimepath")
+-- Don't leave things highlighted after searching
+vim.cmd("set nohlsearch")
 vim.cmd("source ~/.vimrc")
